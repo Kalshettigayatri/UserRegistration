383c7f953b97148989dd385bdcf2d9e784eff265
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-08-11 15:23:43 +0530

    Welcome to user registration

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace UserRegistration
+{
+    internal class UserRegistration
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WElcome to User Registration program");
+            Console.ReadLine();
+        }
+    }
+}
