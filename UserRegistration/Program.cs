56f1564583bc0ad27d61227e5413a94d2f755a9c
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-08-11 15:28:37 +0530

    UC1

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,11 +1,47 @@
 ﻿namespace UserRegistration
 {
-    internal class UserRegistration
-    {
-        static void Main(string[] args)
+    internal class UserRegistrationUC1
+   {
+        class UC1
         {
-            Console.WriteLine("WElcome to User Registration program");
-            Console.ReadLine();
+            public static bool IsValidFirstName(string firstName)
+            {
+                if (string.IsNullOrEmpty(firstName) || firstName.Length < 3)
+                {
+                    return false;
+                }
+
+                if (!char.IsUpper(firstName[0]))
+                {
+                    return false;
+                }
+
+                for (int i = 1; i < firstName.Length; i++)
+                {
+                    if (!char.IsLetter(firstName[i]))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            public static void Main(string[] args)
+            {
+                Console.Write("Enter your first name: ");
+                string firstName = Console.ReadLine();
+
+                if (IsValidFirstName(firstName))
+                {
+                    Console.WriteLine("Valid first name.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid first name. Make sure it starts with a capital letter and has at least 3 characters.");
+                }
+            }
         }
+
     }
 }
